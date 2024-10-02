b339a4605e46aa953f478d7c09d6ecb96300bd9d
Thomas M.R <thorigo2@gmail.com>
2024-10-02 16:18:54 -0300

    Senha para acesso
    1. Foi adicionada a opção para o usuário fazer login no sistema, com uma senha padrão atribuída ao mesmo.
    2. Corrigido os erros provenientes desta mudança.
    3. Alterações na indentação do sistema, para uma visualização melhorada.

--- Cartorio.c
+++ Cartorio.c
@@ -5,11 +5,11 @@
 
                  int registro()
 {
-        char arquivo[40];
-        char cpf[40];
-        char nome[40];
-        char sobrenome[40];
-        char cargo[40];
+                 char arquivo[40];
+                 char cpf[40];
+                 char nome[40];
+                 char sobrenome[40];
+                 char cargo[40];
 
                  printf("Digite o CPF a ser cadastrado:");   //Coleta de informação do usuário
                  scanf("%s", cpf);                          //%s refere-se a string
@@ -57,7 +57,7 @@
                  fprintf(file,cargo);
                  fclose(file);
 
-             system("pause");
+                 system("pause");
 
 
 }
@@ -69,10 +69,10 @@
 					
 		         int consulta()
 {
-        setlocale(LC_ALL,"Portuguese");
+                 setlocale(LC_ALL,"Portuguese");
 				 
-		char cpf[40];
-        char conteudo[200];
+		         char cpf[40];
+                 char conteudo[200];
                  
                  printf("Digite o CPF a ser consultado: ");
                  scanf("%s",cpf);
@@ -82,18 +82,18 @@
                  
                  
 				 if(file == NULL)
-				{
+				 {
 				 printf("Não foi possivel abrir o arquivo, não localizado!. \n");
-				}
+				 }
 				 
 				 
 				 while(fgets(conteudo, 200, file) != NULL)
-			{
+			     {
 				 printf("\nEssas são as informações do usuário: ");
 				 printf("%s", conteudo);
-				 printf("\n\n");
+				 printf("\n\n");				 
+			     }
 				 
-			}
 				 system("pause");
                 
 }
@@ -104,7 +104,7 @@
 
                  int deletar()
 {
-        char cpf[40];
+                 char cpf[40];
     
                  printf("Digitar o CPF a ser deletado: ");
                  scanf("%s", cpf);
@@ -115,33 +115,38 @@
                  file = fopen(cpf,"r");
     
                  if(file == NULL)
-        {
+                 {
                  printf("O usuário não se encontra no sistema!.\n");
 	             system ("pause");      
-             }
+                 }
 }
 
 
 
-int main()
-                {
-	                int opcao=0;         //Definindo variáveis
-	                int x=1;
-	                
-					for(x=1;x=1;)
-                {
+                     int main()
+                     {
+	                 int opcao=0;         //Definindo variáveis
+	                 int x=1;	                
+					 char senhadigitada[]="a";					
+					 int comparacao;
+					
+					 printf("### Cartório da EBAC ###\n\n");
+				 	 printf("Login de administrador!\n\nDigite a sua senha: ");
+					 scanf("%s",senhadigitada);
+					
+					 comparacao = strcmp(senhadigitada, "admin");
+					
+					 if(comparacao == 0)
+					
+					
+					 for(x=1;x=1;)
+    {
 								
-					system("cls");	                
-					setlocale(LC_ALL,"Portuguese");  //Definindo a linguagem	
-
-
-
-                    	
+					 system("cls");	                
+					 setlocale(LC_ALL,"Portuguese");  //Definindo a linguagem	                   
 //inicio do menu 						
 						printf("### Cartório da EBAC ###\n\n");
-	        
 			            printf("Escolha a opção desejada do menu:\n\n");
-	                  
 					    printf("\t1 - Registrar nomes\n");
 	                    printf("\t2 - Consultar nomes\n");
 					    printf("\t3 - Deletar nomes\n\n");
@@ -153,46 +158,39 @@ int main()
 	                    
 					    system("cls");        //Responsavel por limpar a tela
 	                    
-	                switch(opcao)          //Inicio da seleção
-				{
-					case 1:
-					registro();      //Chama de funções              
-					break;
+	                 switch(opcao)          //Inicio da seleção
+				     {
+					 case 1:
+					 registro();      //Chama de funções              
+					 break;
 					
-					case 2:
-					consulta();
-					break;
+					 case 2:
+					 consulta();
+					 break;
 				
-				    case 3:
-					deletar();
-					break;
+				     case 3:
+					 deletar();
+					 break;
 					
-					case 4:
-					printf("Obrigado por ultizar o sistema!\n");
-					return 0;
-					break;
+					 case 4:
+					 printf("Obrigado por ultizar o sistema!\n");
+					 return 0;
+					 break;
 
-					default:
-					printf("Essa opção não existe\n");
-					system("pause");
-				    break;
-				}
-				    
-				    
-}
+					 default:
+					 printf("Essa opção não existe\n");
+				 	 system("pause");
+				     break;
+				     }
+	          			    
+				  		
+     }
+						
+			
+					 else
+		             printf("Senha Incorreta");	  
 						
 						
 						
-						
-						
-						
-					
-
-
-
-
-
-
-
 }
 
